ffbbb81386e235d6fe771fbe6e8a5cf095a43fc4
IamsadVN <112055678+IamsadVN@users.noreply.github.com>
2025-07-03 21:01:22 +0700

    add 'dist' into .gitignore

--- api/dist/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { config } from "dotenv";
-import { Logger } from "./utils/index.js";
-config();
-const app = express();
-app.listen(process.env.PORT, () => {
-    Logger.info(`Server is running on port ${process.env.PORT}`);
-});
