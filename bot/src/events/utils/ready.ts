f1eee8367a8a9d1e0c2f131fa5f4c781d9ddb056
IamsadVN <112055678+IamsadVN@users.noreply.github.com>
2025-07-07 23:40:44 +0700

    change status bot

--- bot/src/events/utils/ready.ts
+++ bot/src/events/utils/ready.ts
@@ -10,9 +10,21 @@ export default class Ready extends Listener<"ready"> {
     public async execute(client: Client<true>) {
         botLogger.info(`Logged with name ${client.user.username}`);
 
-        client.user.setActivity({
-            name: "Nhìn cái gì, nghe nhạc không?",
-            type: ActivityType.Listening
+        const totalGuilds = client.guilds.cache.size.toLocaleString("es-ES");
+
+        client.user.setPresence({
+            status: "idle",
+            activities: [
+                {
+                    type: ActivityType.Custom,
+                    name: " ",
+                    state: "Nhìn cái gì, nghe nhạc không?"
+                },
+                {
+                    type: ActivityType.Listening,
+                    name: `Đang nghe nhạc với ${totalGuilds} server ♥`
+                }
+            ]
         })
     }
 }
